4314bc6674dada4f6cab90e23e362bcb6dad2636
ossama <ossamamahmoudy@gmail.com>
2021-01-14 11:14:30 +0100

    step1 / ex 2

---
+++ ex2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	
+	int a , b ;
+
+		    
+		   
+			printf("donnez les deux nombres \n");
+			
+	        scanf("%d %d",&a,&b);
+	        
+	        printf("l'addition de deux nombres est %d + %d = %d \n",a,b,a+b);
+	        
+	        printf("la resultat de la soustraction est %d - %d = %d \n ",a,b,a-b);
+	        
+	        printf("la resultat de la multiplication est %d * %d = %d \n ",a,b,a*b);
+	        
+	        printf("la resultat de la division est %d / %d = %d \n ",a,b,a/b);
+	         
+	        printf("la resultat du modulo est %d mod %d = %d \n ",a,b,a%b);
+	}
+		
+	
+
+	 
+	    
+	
+
