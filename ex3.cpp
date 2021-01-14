ea02902fc25239fc87086dbf3e9800818dba47b7
ossama <ossamamahmoudy@gmail.com>
2021-01-14 11:17:37 +0100

    step1 / ex3

---
+++ ex3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+
+int main(){
+	    const  float pi=3;
+	    float r , D , S ,P;
+	    
+	         printf("Donnez r ");
+	         scanf("%f",&r);
+	    
+	    D = r*2;
+	    P = 2*pi*r;
+	    S = pi*r*r;
+	    
+	         printf("Le diamétre est %f",D);
+	         printf("Le périmétre est %f",P);
+	         printf("la surface est %f",S);
+	    
+
+	  
+}
