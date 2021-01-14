36c3c990d53460665ffa46448e80209014e34235
ossama <ossamamahmoudy@gmail.com>
2021-01-14 11:37:29 +0100

    challenge*

---
+++ Challenge.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+	int nmbr , i ,t,j ;
+	
+	    printf("Donnez un nombre : ");
+	    scanf("%d",&nmbr);
+	    
+	   
+	    
+			
+	      while(j<=10)
+	    {	
+	    
+	    	t = nmbr*j;		
+	    	printf("%d * %d  = %d \n" ,nmbr,j,t);
+	    	j++;
+	    	
+		}
+	
+	
+
+}
