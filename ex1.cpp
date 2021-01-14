74ee7a9b58a3da4073eca7a37cedb5f67739f3ff
ossama <ossamamahmoudy@gmail.com>
2021-01-14 11:00:54 +0100

    step 1 / ex1

---
+++ ex1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h> 
+ main ()
+ {
+ 	char nom [10], Prenom[10] , sexe[10];
+ 	int age , num_tel ;
+ 	
+ 	      
+ 	    printf("Entrer votre nom \n");
+ 	    scanf("%s",&nom);
+ 	    printf("Entrer votre Prenom \n");
+ 	    scanf("%s",&Prenom);
+ 	    printf("Entrer votre sexe \n");
+ 	    scanf("%s",&sexe);
+ 	    printf("Entrer votre age \n");
+ 	    scanf("%s",&nom);
+ 	    printf("Entrer votre age \n");
+ 	    scanf("%d",&age);
+ 	    printf("Entrer votre num_tel \n");
+ 	    scanf("%d",&num_tel);
+ 	    
+ 	    printf("tes infos sont %s %s %s %d %d \n ",nom,Prenom,sexe,age,num_tel);
+ }
